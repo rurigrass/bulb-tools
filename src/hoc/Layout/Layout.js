9bef436e8c08039cc697ea8010e3bbf63d81b735
rurigrass <ruripg@gmail.com>
2020-04-25 22:25:31 +0100

    i added the bulb logo to the header

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -2,16 +2,25 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
+import logo from '../../images/bulb-logo-white.png'
+
 import Aux from '../Aux/Aux';
 
 const Header = styled.header`
+    display: flex;
+    align-items: center;
     height: 3.5rem;
     background-color: #001A4D;
+`
+
+const HeaderLogo = styled.img`
+    width: 5rem;
+    margin-left: 1rem;
 `
 const Layout = (props) => {
     return (
         <Aux>
-            <Header><Link to="/">Bulb</Link></Header>
+            <Header><Link to="/"><HeaderLogo src={logo} /></Link></Header>
             <main>{props.children}</main>
         </Aux>
     );
