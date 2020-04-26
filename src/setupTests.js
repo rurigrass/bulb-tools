4aafcb0f72d40444e0c251845c8d9fcf5f8b4271
rurigrass <ruripg@gmail.com>
2020-04-26 17:35:40 +0100

    trying to make this work with run dev

--- src/setupTests.js
+++ src/setupTests.js
@@ -3,3 +3,10 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom/extend-expect';
+
+
+//THIS IS THE OLD "SCRIPTS" PACKAGE>JSON
+// "start": "react-scripts start",
+// "build": "react-scripts build",
+// "test": "react-scripts test",
+// "eject": "react-scripts eject"
