1832e56d1a9e9703d50d5daafd5036f03cacc752
rurigrass <ruripg@gmail.com>
2020-05-09 18:15:57 +0100

    added bold to tables tariff finder and dispute calculator

--- src/components/DisputeCalculator/DisputeCalculatorResult.js
+++ src/components/DisputeCalculator/DisputeCalculatorResult.js
@@ -17,32 +17,32 @@ const DisputeCalculatorResult = (props) => {
 
     if (disputeReadingNight) {
         disputedReading =
-        <div className="ui segment">
-        <table className="ui very basic collapsing celled table">
-            <thead>
-                <tr>
-                    <th></th>
-                    <th>Read on {format(new Date(disputeDate), "dd/MM/yy")}</th>
-                    <th>Avg Day</th>
-                    <th>EAC</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Day</td>
-                    <td>{disputeReadingDay.toFixed(0)}{unit}</td>
-                    <td>{energyOneDay.toFixed(2)}{unit}</td>
-                    <td>{(energyOneDay * 365).toFixed(0)}{unit}</td>
-                </tr>
-                <tr>
-                    <td>Night</td>
-                    <td>{disputeReadingNight.toFixed(0)}{unit}</td>
-                    <td>{energyOneNight.toFixed(2)}{unit}</td>
-                    <td>{(energyOneNight * 365).toFixed(0)}{unit}</td>
-                </tr>
-            </tbody>
-        </table>
-    </div>
+            <div className="ui segment">
+                <table className="ui very basic collapsing celled table">
+                    <thead>
+                        <tr>
+                            <th></th>
+                            <th>Read on {format(new Date(disputeDate), "dd/MM/yy")}</th>
+                            <th>Avg Day</th>
+                            <th>EAC</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td><b>Day</b></td>
+                            <td>{disputeReadingDay.toFixed(0)}{unit}</td>
+                            <td>{energyOneDay.toFixed(2)}{unit}</td>
+                            <td>{(energyOneDay * 365).toFixed(0)}{unit}</td>
+                        </tr>
+                        <tr>
+                            <td><b>Night</b></td>
+                            <td>{disputeReadingNight.toFixed(0)}{unit}</td>
+                            <td>{energyOneNight.toFixed(2)}{unit}</td>
+                            <td>{(energyOneNight * 365).toFixed(0)}{unit}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
     } else {
         disputedReading =
             <div className="ui segment">
--- src/components/TariffFinder/TariffFinderResult.js
+++ src/components/TariffFinder/TariffFinderResult.js
@@ -16,7 +16,7 @@ const TariffFinderResult = (props) => {
     if (meterRate === "1 rate") {
         elecUnitRate =
             <tr>
-                <td data-label="Unit Rate">Unit Rate</td>
+                <td data-label="Unit Rate"><b>Unit Rate</b></td>
                 <td data-label="Energy Cost">{exclVAT(tariff.residential.current[meterType].elec.oneRate * 100).toFixed(4)}p per kWh</td>
                 <td data-label="Energy Cost">{(tariff.residential.current[meterType].elec.oneRate * 100).toFixed(4)}p per kWh</td>
             </tr>
@@ -24,7 +24,7 @@ const TariffFinderResult = (props) => {
         elecUnitRate = Object.keys(tariff.residential.current[meterType].elec.twoRate).map(rate => {
             return (
                 <tr key={rate}>
-                    <td data-label="Unit Rate">{rate.charAt(0).toUpperCase() + rate.slice(1)} Rate</td>
+                    <td data-label="Unit Rate"><b>{rate.charAt(0).toUpperCase() + rate.slice(1)}Rate</b></td>
                     <td data-label="Energy Cost">{exclVAT(tariff.residential.current[meterType].elec.twoRate[rate] * 100).toFixed(4)}p per kWh</td>
                     <td data-label="Energy Cost">{(tariff.residential.current[meterType].elec.twoRate[rate] * 100).toFixed(4)}p per kWh</td>
                 </tr>
@@ -50,7 +50,7 @@ const TariffFinderResult = (props) => {
                     <tbody>
                         {elecUnitRate}
                         <tr>
-                            <td data-label="Unit Rate">Standing Charge</td>
+                            <td data-label="Unit Rate"><b>Standing Charge</b></td>
                             <td data-label="Energy Cost">{exclVAT(tariff.residential.current[meterType].elec.standing * 100).toFixed(3)}p per day (£{exclVAT(tariff.residential.current[meterType].elec.standing * 365).toFixed(2)} per year)</td>
                             <td data-label="Energy Cost">{(tariff.residential.current[meterType].elec.standing * 100).toFixed(3)}p per day (£{(tariff.residential.current[meterType].elec.standing * 365).toFixed(2)} per year)</td>
                         </tr>
@@ -71,12 +71,12 @@ const TariffFinderResult = (props) => {
                     </thead>
                     <tbody>
                         <tr>
-                            <td data-label="Unit Rate">Unit Rate</td>
+                            <td data-label="Unit Rate"><b>Unit Rate</b></td>
                             <td data-label="Energy Cost">{exclVAT(tariff.residential.current[meterType].gas.oneRate * 100).toFixed(4)}p per kWh</td>
                             <td data-label="Energy Cost">{exclVAT(tariff.residential.current[meterType].gas.oneRate * 100).toFixed(4)}p per kWh</td>
                         </tr>
                         <tr>
-                            <td data-label="Unit Rate">Standing Charge</td>
+                            <td data-label="Unit Rate"><b>Standing Charge</b></td>
                             <td data-label="Energy Cost">{exclVAT(tariff.residential.current[meterType].gas.standing * 100).toFixed(3)}p per day (£{exclVAT(tariff.residential.current[meterType].elec.standing * 365).toFixed(2)} per year)</td>
                             <td data-label="Energy Cost">{(tariff.residential.current[meterType].gas.standing * 100).toFixed(3)}p per day (£{(tariff.residential.current[meterType].elec.standing * 365).toFixed(2)} per year)</td>
                         </tr>
