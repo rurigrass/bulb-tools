b0f827ae7bb57574898351c6f8267577344b7659
rurigrass <ruripg@gmail.com>
2020-05-13 21:52:50 +0100

    tried with ga in indesx.js

--- src/index.js
+++ src/index.js
@@ -6,11 +6,28 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const injectGA = () => {
+  if (typeof window == 'undefined') {
+    return;
+  }
+  window.dataLayer = window.dataLayer || [];
+  function gtag() {
+    window.dataLayer.push(arguments);
+  }
+  gtag('js', new Date());
+
+  gtag('config', 'UA-166320614-1');
+};
+
+
 const app = (
   <BrowserRouter>
-    {/* <React.StrictMode> */}
+    <script
+      async
+      src="https://www.googletagmanager.com/gtag/js?id=UA-166320614-1"
+    />
+    <script>{injectGA()}</script>
     <App />
-    {/* </React.StrictMode> */}
   </BrowserRouter>
 )
 
