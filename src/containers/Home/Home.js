8963761b1ff36b6bc3d95b2a1d9ef209baa2ca91
rurigrass <ruripg@gmail.com>
2020-06-01 01:17:42 +0100

    toolkit deployed to biulb git

--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,25 +1,25 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import styled from 'styled-components';
+import React from "react";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const Wrapper = styled.div`
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;  
-`
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
 
 const Card = styled.div`
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background-color: #01AB55;
-    margin: 1.5em;
-    width: 15em;
-    height: 15em;
-    border-radius: 15px;
-    cursor: pointer;
-    color: #fff;
-`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: #01ab55;
+  margin: 1.5em;
+  width: 15em;
+  height: 15em;
+  border-radius: 15px;
+  cursor: pointer;
+  color: #fff;
+`;
 
 // I PLAN TO INSERT THE IMAGES
 // const CardImage = styled.img`
@@ -27,14 +27,12 @@ const Card = styled.div`
 //     margin-left: 1em;
 // `
 const Home = () => {
-    return (
-        <Wrapper>
-            <a href="https://es-toolkit.bulb.com/">
-                <Card>
-                    New Link for ES Toolkit
-                </Card>
-            </a>
-            {/* <Link to='/bill-calculator'>
+  return (
+    <Wrapper>
+      <a href="https://es-toolkit.bulb.com/">
+        <Card>ES Toolkit New Link</Card>
+      </a>
+      {/* <Link to='/bill-calculator'>
                 <Card>
                     Bill Calculator
                 </Card>
@@ -64,8 +62,8 @@ const Home = () => {
                     Business
                 </Card>
             </Link> */}
-        </Wrapper>
-    );
+    </Wrapper>
+  );
 };
 
 export default Home;
